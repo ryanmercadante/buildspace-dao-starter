c72a0652a9e274c057b60a1ac03cd2011b31e9ba
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-12-30 21:31:24 -0500

    render member data on DAO dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -146,6 +146,29 @@ const App = () => {
       <div className='member-page'>
         <h1>🍪DAO Member Page</h1>
         <p>Congratulations on being a member</p>
+        <div>
+          <div>
+            <h2>Member List</h2>
+            <table className='card'>
+              <thead>
+                <tr>
+                  <th>Address</th>
+                  <th>Token Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+                {memberList.map((member) => {
+                  return (
+                    <tr key={member.address}>
+                      <td>{shortenAddress(member.address)}</td>
+                      <td>{member.tokenAmount}</td>
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     )
   }
